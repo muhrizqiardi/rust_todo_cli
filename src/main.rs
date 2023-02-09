90136856d46c87add5a6d228b30a3ade9666282a
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-02-09 13:33:06 +0700

    refactor: change if-else to match statements

--- src/main.rs
+++ src/main.rs
@@ -7,60 +7,43 @@ struct Todo {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
+async fn main() {
     let args = std::env::args().collect::<Vec<String>>();
 
-    if args[1] == "list" || args[1] == "l" {
-        let _placeholder_list = [
-            Todo {
-                id: 1,
-                content: String::from("Do laundry"),
-                checked: false,
-            },
-            Todo {
-                id: 2,
-                content: String::from("Listen to In Rainbows by Radiohead"),
-                checked: true,
-            },
-            Todo {
-                id: 3,
-                content: String::from("Do dishes"),
-                checked: false,
-            },
-        ];
-
-        for item in _placeholder_list {
-            println!(
-                "{}. [{}] {}",
-                item.id,
-                if item.checked { "x" } else { " " },
-                item.content,
-            )
-        }
-    }
-
-    if args[1] == "add" || args[1] == "a" {
-        println!("Added \"{}\" with id \"{}\"", &args[2], 1);
-    }
-
-    if args[1] == "delete" || args[1] == "d" {
-        println!("Deleted \"{}\" with id \"{}\"", "placeholder", 1);
-    }
-
-    if args[1] == "check" || args[1] == "c" {
-        println!("Checked \"{}\" with id \"{}\"", "placeholder", 1);
-    }
-
-    if args[1] == "uncheck" || args[1] == "u" {
-        println!("Unchecked \"{}\" with id \"{}\"", "placeholder", 1);
-    }
-
-    if args[1] == "edit" || args[1] == "e" {
-        println!(
+    match args[1].as_str() {
+        "add" | "a" => println!("Added \"{}\" with id \"{}\"", &args[2], 1),
+        "delete" | "d" => println!("Deleted \"{}\" with id \"{}\"", "placeholder", 1),
+        "check" | "c" => println!("Deleted \"{}\" with id \"{}\"", "placeholder", 1),
+        "uncheck" | "u" => println!("Unchecked \"{}\" with id \"{}\"", "placeholder", 1),
+        "edit" | "e" => println!(
             "Changed from \"{}\" to \"{}\" with id \"{}\"",
             "placeholder 1", "placeholder 2", 1
-        );
+        ),
+        "list" | "l" | _ => {
+            for item in [
+                Todo {
+                    id: 1,
+                    content: String::from("Do laundry"),
+                    checked: false,
+                },
+                Todo {
+                    id: 2,
+                    content: String::from("Listen to In Rainbows by Radiohead"),
+                    checked: true,
+                },
+                Todo {
+                    id: 3,
+                    content: String::from("Do dishes"),
+                    checked: false,
+                },
+            ] {
+                println!(
+                    "{}. [{}] {}",
+                    item.id,
+                    if item.checked { "x" } else { " " },
+                    item.content,
+                )
+            }
+        }
     }
-
-    Ok(())
 }
